3c78a8923f364bccb78c1ca15d12e2c6ac864381
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-09-28 13:40:36 +0200

    feat(static): redirect github pages to custom domain

--- env-config.js
+++ env-config.js
@@ -1,9 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.PREFIX': prod
-    ? '/deepavatar.xyz'
-    : '',
+  'process.env.PREFIX': '',
   'process.env.BACKEND_URL': prod
     ? 'https://deepavatar-api-production.herokuapp.com'
     : 'http://localhost:3010',
--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = withCSS({
       '/': { page: '/' },
       '/create': { page: '/create' },
     };
-  },
-  assetPrefix: debug ? './' : `/deepavatar.xyz`,
+ },
 });
 
