7cd442dd54bf6fa44f1aef53b949a3fe2da657ba
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-10-03 15:45:55 +0200

    fix(meta): set image with domain

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,11 @@ export default class extends Document {
       <html>
         <Head>
           <title>{title}</title>
-          <meta name="description" content={description}></meta>
-          <meta property="og:title" content={title}></meta>
-          <meta property="og:site_name" content={title}></meta>
-          <meta property="og:description" content={description}></meta>
-          <meta property="og:image" content={Static.url('/static/cat-transformation.jpg')}></meta>
+          <meta name="description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta property="og:site_name" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content="https://www.deepavatar.xyz/static/cat-transformation.jpg" />
           <link rel="icon" href={Static.url('/static/favicon.jpg')}></link>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Varela+Round|Roboto:400,500,700" rel="stylesheet" />
