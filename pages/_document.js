e58a88d994ea29d2004f92d5fc44e9bc43237a4d
Jérémie Drouet <jeremie.drouet@gmail.com>
2018-10-03 15:30:58 +0200

    feat(meta): add open graph meta data

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import Static from '../services/static';
 
+const title = 'deepavatar';
+const description = 'Make your avatar unique with the help of Artificial Intelligence!';
+
 export default class extends Document {
   render() {
     return (
       <html>
         <Head>
-          <title>deepavatar</title>
-          <meta name="description" content="Make your avatar unique with the help of Artificial Intelligence!"></meta>
-          <meta property="og:image" content={Static.url('/static/cat-social.jpg')}></meta>
+          <title>{title}</title>
+          <meta name="description" content={description}></meta>
+          <meta property="og:title" content={title}></meta>
+          <meta property="og:site_name" content={title}></meta>
+          <meta property="og:description" content={description}></meta>
+          <meta property="og:image" content={Static.url('/static/cat-transformation.jpg')}></meta>
           <link rel="icon" href={Static.url('/static/favicon.jpg')}></link>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Varela+Round|Roboto:400,500,700" rel="stylesheet" />
